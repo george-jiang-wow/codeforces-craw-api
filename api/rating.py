a867f925e94b7649c105c3d2708edb6fbb1fc572
george <453617479@qq.com>
2023-06-21 12:02:25 +0800

    update

---
+++ api/rating.py
@@ -0,0 +1,34 @@
+import io
+import json
+import requests
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib import parse
+from PIL import Image, ImageDraw, ImageFont
+class handler(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        if self.path.startswith('/rating'):
+            params = parse.parse_qs(parse.urlparse(self.path).query)
+            user_wants_data = params.get('data', [''])[0]
+            self.send_response(200)
+            self.send_header('Content-Type', 'image/jpeg')
+            self.end_headers()
+            usersetting=user_wants_data
+            url = 'https://www.spoj.com/users/'+usersetting+'/'
+            response = requests.get(url)
+            txt1 = response.text
+            problem_sloved=txt1.split("Problems solved</dt>\n\t\t\t\t\t\t\t\t<dd>")[1].split("</dd>")[0]
+            solution_accepted=txt1.split("Solutions submitted</dt>\n\t\t\t\t\t\t\t\t<dd>")[1].split("</dd>")[0]
+            font_path = './data/font.ttf'
+            font = ImageFont.truetype(font_path, 32)
+            img_size = (600, 400)
+            img_color = (255, 255, 255)
+            img_text = f"AC: {problem_sloved} ; Solution: {solution_accepted}" 
+            img = Image.new('RGB', img_size, img_color)
+            draw = ImageDraw.Draw(img)
+            draw.text((50, 50), img_text, font=font, fill=(0, 0, 0))
+            buffered = io.BytesIO()
+            img.save(buffered, format="JPEG")
+            img_byte = buffered.getvalue()
+            self.wfile.write(img_byte)
+            return
