6510950e61099b9e8242e4915cf1187c8453a06b
george-jiang-wow <453617479@qq.com>
2023-07-11 00:42:26 +0000

    update

--- api/rating.py
+++ api/rating.py
@@ -7,28 +7,21 @@ from PIL import Image, ImageDraw, ImageFont
 class handler(BaseHTTPRequestHandler):
     
     def do_GET(self):
-        if self.path.startswith('/rating'):
+        if self.path.startswith('/code'):
             params = parse.parse_qs(parse.urlparse(self.path).query)
             user_wants_data = params.get('data', [''])[0]
             self.send_response(200)
             self.send_header('Content-Type', 'image/jpeg')
             self.end_headers()
+            cookies={"__client_id":token,"_uid":"395758"}
+            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.47 BOTE"}
+            re=r.get("https://www.luogu.com.cn/user/395758",headers=headers,cookies=cookies)
+            headers["X-CSRF-TOKEN"]=re.text.split('<meta name="X-Csrf-Token" content="')[1].split('">')[0]
             usersetting=user_wants_data
-            url = 'https://www.spoj.com/users/'+usersetting+'/'
-            response = requests.get(url)
+            payload = {'submissionId': usersetting, 'csrf_token': re.text.split('<meta name="X-Csrf-Token" content="')[1].split('">')[0]}
+            url = 'https://codeforc.es/data/submitSource'
+            response = requests.post(url,headers=headers,cookies=cookies,data=payload)
             txt1 = response.text
-            problem_sloved=txt1.split("Problems solved</dt>\n\t\t\t\t\t\t\t\t<dd>")[1].split("</dd>")[0]
-            solution_accepted=txt1.split("Solutions submitted</dt>\n\t\t\t\t\t\t\t\t<dd>")[1].split("</dd>")[0]
-            font_path = './data/font.ttf'
-            font = ImageFont.truetype(font_path, 20)
-            img_size = (20*len(problem_sloved)+92, 28)
-            img_color = (255, 255, 255)
-            img_text = f"AC: {problem_sloved} ; Push: {solution_accepted}" 
-            img = Image.new('RGB', img_size, img_color)
-            draw = ImageDraw.Draw(img)
-            draw.text((5, 5), img_text, font=font, fill=(0, 0, 0))
-            buffered = io.BytesIO()
-            img.save(buffered, format="JPEG")
-            img_byte = buffered.getvalue()
-            self.wfile.write(img_byte)
+            
+            self.wfile.write(ans)
             return
