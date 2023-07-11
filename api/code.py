145cf318cf29d50ba5c138b44b5e80007ecc4a00
george-jiang-wow <453617479@qq.com>
2023-07-11 00:51:10 +0000

    1

--- api/code.py
+++ api/code.py
@@ -13,14 +13,13 @@ class handler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-Type', 'image/jpeg')
             self.end_headers()
-            cookies={"__client_id":token,"_uid":"395758"}
             headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.47 BOTE"}
-            re=r.get("https://www.luogu.com.cn/user/395758",headers=headers,cookies=cookies)
+            re=r.get("https://codeforc.es/problemset/status",headers=headers)
             headers["X-CSRF-TOKEN"]=re.text.split('<meta name="X-Csrf-Token" content="')[1].split('">')[0]
             usersetting=user_wants_data
             payload = {'submissionId': usersetting, 'csrf_token': re.text.split('<meta name="X-Csrf-Token" content="')[1].split('">')[0]}
             url = 'https://codeforc.es/data/submitSource'
-            response = requests.post(url,headers=headers,cookies=cookies,data=payload)
+            response = requests.post(url,headers=headers,data=payload)
             txt1 = response.text
             ans=txt1
             self.wfile.write(ans)
